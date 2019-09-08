47ae270f11d40c46af7399f0254d1a2acb692a6e
Mohamad Mujeeb Urahaman <mohamadmujeeb.urahaman@photoninfotech.net>
2019-09-08 15:47:27 +0530

    added is tablet check for app

--- app/src/main/java/com/techno/expensetracker/ApplicationExpTracker.java
+++ app/src/main/java/com/techno/expensetracker/ApplicationExpTracker.java
@@ -2,5 +2,24 @@ package com.techno.expensetracker;
 
 import android.app.Application;
 
+import com.techno.expensetracker.Utils.Constants;
+
 public class ApplicationExpTracker extends Application {
+
+    private static ApplicationExpTracker mSelf;
+
+    public ApplicationExpTracker() {
+        mSelf = this;
+    }
+
+    public static ApplicationExpTracker getInstance() {
+        return mSelf;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        Constants.IS_TABLET = getResources().getBoolean(R.bool.isTablet);
+    }
 }
