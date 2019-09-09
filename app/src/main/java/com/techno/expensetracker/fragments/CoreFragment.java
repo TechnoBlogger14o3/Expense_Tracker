577ac37d0bce948330c7b73c31b848c0781c0aa3
Mohamad Mujeeb Urahaman <mohamadmujeeb.urahaman@photoninfotech.net>
2019-09-09 11:38:45 +0530

    added methods for bottombar

--- app/src/main/java/com/techno/expensetracker/fragments/CoreFragment.java
+++ app/src/main/java/com/techno/expensetracker/fragments/CoreFragment.java
@@ -3,9 +3,9 @@ package com.techno.expensetracker.fragments;
 
 import android.content.Context;
 
-import androidx.fragment.app.Fragment;
-
 import com.techno.expensetracker.Activities.MainActivity;
+
+import androidx.fragment.app.Fragment;
 
 public class CoreFragment extends Fragment {
 
@@ -33,6 +33,18 @@ public class CoreFragment extends Fragment {
     public void hideToolBar() {
         if (mActivity != null) {
             mActivity.hideToolBar();
+        }
+    }
+
+    public void showBottomBar() {
+        if (mActivity != null) {
+            mActivity.showBottomBar();
+        }
+    }
+
+    public void hideBottomBar() {
+        if (mActivity != null) {
+            mActivity.hideBottomBar();
         }
     }
 }
